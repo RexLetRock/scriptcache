498b63702d442db480a584668012480fa9428b38
RexLetRock <rexletrock@gmail.com>
2022-08-30 23:25:40 +0700

    20220830:232539

--- tcp/client.go
+++ tcp/client.go
@@ -9,15 +9,15 @@ import (
 const NCpu = 12
 const NRun = 1_000_000
 
+var conns = [NCpu]net.Conn{}
+
 func ClientStart() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8888")
-	if err != nil {
-		panic(err)
+	for i := 0; i < NCpu; i++ {
+		conns[i], _ = net.Dial("tcp", "127.0.0.1:8888")
 	}
 
 	a := []byte("FUCK THIS SHIT IAM DONE")
 	zbench.Run(1_000_000, 20, func(i, thread int) {
-		conn.Write(a)
+		conns[thread].Write(a)
 	})
-	defer conn.Close()
 }
