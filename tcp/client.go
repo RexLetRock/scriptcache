4dbed8bb9c501ec0727aeebc91d6c2e04f983710
RexLetRock <rexletrock@gmail.com>
2022-08-31 09:58:25 +0700

    20220831:095824

--- tcp/client.go
+++ tcp/client.go
@@ -1,37 +1,31 @@
 package tcp
 
 import (
-	"bufio"
-	"io"
-	"io/ioutil"
 	"net"
 
 	"github.com/RexLetRock/zlib/zbench"
 )
 
-const NCpu = 30
-const NRun = 3_000_000
+const NCpu = 12
+const NRun = 1_000_000
 
 var conns = [NCpu]net.Conn{}
+var chans = [NCpu]chan ([]byte){}
 
 func ClientStart() {
 	for i := 0; i < NCpu; i++ {
 		conns[i], _ = net.Dial("tcp", "127.0.0.1:8888")
+		chans[i] = make(chan []byte, 1024*1000)
+		go func(i int) {
+			for {
+				msg := <-chans[i]
+				conns[i].Write(msg)
+			}
+		}(i)
 	}
 
 	a := []byte("How are you today :D \n")
-
-	pipeReader, pipeWriter := io.Pipe()
-	reader := bufio.NewReader(pipeReader)
-	go func() {
-		for {
-			data, _ := ioutil.ReadAll(reader)
-			conns[0].Write(data)
-		}
-	}()
-
 	zbench.Run(NRun, NCpu, func(i, thread int) {
-		// conns[thread].Write(a)
-		pipeWriter.Write(a)
+		chans[thread] <- a
 	})
 }
