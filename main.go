f4702ea304a78dee92567b829df3e0d5445316a9
RexLetRock <rexletrock@gmail.com>
2022-09-13 00:41:21 +0700

    20220913:004121

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/RexLetRock/scriptcache/tcp"
+	"github.com/RexLetRock/scriptcache/zevio"
 )
 
 const connHost = "127.0.0.1:9000"
@@ -12,8 +13,8 @@ func main() {
 	// go tcp.ServerStartViaOptions(connHost)
 	// go zevio.MainEvio()
 	// go zgnet.MainGnet()
-	go tcp.ServerStart()
-	time.Sleep(3 * time.Second)
+	go zevio.MainEvio()
+	time.Sleep(2 * time.Second)
 	tcp.ClientStart(connHost)
 	select {}
 }
