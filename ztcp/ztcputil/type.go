bb3853831a707e6f317e6389249855370c1839e5
RexLetRock <rexletrock@gmail.com>
2022-09-14 10:45:34 +0700

    20220914:104534

--- ztcp/ztcputil/type.go
+++ ztcp/ztcputil/type.go
@@ -2,10 +2,10 @@ package ztcputil
 
 import "time"
 
-const NCpu = 12
+const NCpu = 60
 const NRun = 30_000_000
 const ENDLINE = "#\t#"
 
 const SendSize = 10_000
 const ChansSize = 1024 * 100
-const TimeToFlush = 1000 * time.Microsecond
+const TimeToFlush = 100 * time.Microsecond
