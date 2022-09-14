322a61f2a458166a9e97e8ee3ab7c325646e093b
RexLetRock <rexletrock@gmail.com>
2022-09-14 10:44:30 +0700

    20220914:104430

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"time"
 
+	"github.com/RexLetRock/scriptcache/zevio"
 	"github.com/RexLetRock/scriptcache/ztcp/ztcpclient"
-	"github.com/RexLetRock/scriptcache/ztcp/ztcpserver"
 )
 
-const connHost = "127.0.0.1:19999"
+const connHost = "127.0.0.1:9000"
 
 func main() {
 	// go zgnet.MainGnet()
-	// go zevio.MainEvio()
+	go zevio.MainEvio()
 
-	go ztcpserver.ServerStartViaOptions(connHost)
+	// go ztcpserver.ServerStartViaOptions(connHost)
 	time.Sleep(2 * time.Second)
 	ztcpclient.ClientStart(connHost)
 
